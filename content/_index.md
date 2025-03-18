f01b0115e3b5c225944f8c819659401bcc642e1a
quintuple_lained <quintuple_lained@proton.me>
2025-03-18 12:31:24 +0100

    added theme and inital page

---
+++ content/_index.md
@@ -0,0 +1,6 @@
++++
+title = "The snible"
+type = "home"
++++
+
+this is where you will soon find the holy scripture of the Snurch
---
+++ public/searchindex.en.js
@@ -0,0 +1,26 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "The snible",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "The snible",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "welcome to the Snible this is where you will soon find the holy scripture of the Snurch",
+    "description": "welcome to the Snible this is where you will soon find the holy scripture of the Snurch",
+    "tags": [],
+    "title": "The snible",
+    "uri": "/index.html"
+  }
+]
